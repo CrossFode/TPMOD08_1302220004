78d5e1560ca4a252662582d363a044beffb7b6bc
CrossFode <149864968+CrossFode@users.noreply.github.com>
2024-04-15 00:28:14 +0700

    Menyelesaikan project dengan runtime configuration

--- TPMOD8/obj/Debug/net8.0/TPMOD8.AssemblyInfo.cs
+++ TPMOD8/obj/Debug/net8.0/TPMOD8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TPMOD8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53092715c74b5a0fd23260debced4522eafcbe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TPMOD8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TPMOD8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
